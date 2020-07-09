d0e79a6788d2db1ab459148373cff46f3f5ed620
Chindalath Traymany <chindalatht@gmail.com>
2020-07-09 06:13:15 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -3,8 +3,14 @@ learn spec/01_challenge_spec.rb --f-f
 
 def first_challenge
    epic_tragedy = {
-     montague: {},
-     capulet: {}
+     montague: {
+       
+     },
+     
+     
+     capulet: {
+       
+     }
      
    }
 end
